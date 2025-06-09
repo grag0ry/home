74c6b38a10e61797691ab197f8314a02fe996ce4
Gregory Kudrenko <gragory.mail@gmail.com>
2025-06-09 02:00:23 +0200

    Improve makeconf

--- tools/makeconf.sh
+++ tools/makeconf.sh
@@ -5,9 +5,6 @@ set -e -u -o pipefail
 
 : "${CFG_OS:=$(uname -s)}"
 
-: "${CFG_WSL:=}"
-[[ ! -v CFG_WSL && $(uname -r) = *-microsoft-* ]] && CFG_WSL=1
-
 if [[ -n ${CFG_OSID:-} ]]; then
     true
 elif [[ $(uname -s) = *CYGWIN* ]]; then
@@ -19,10 +16,9 @@ fi
 : "${CFG_HOME:=$HOME}"
 CFG_HOME=$(realpath -m "$CFG_HOME")
 
-: "${CFG_NVIM:=}"
-[[ -z $CFG_NVIM && -n $(command -v nvim) ]] && CFG_NVIM=1
-
-: "${CFG_GNUPG_AGENT:=}"
+: "${CFG_WSL=$([[ $(uname -r) = *-microsoft-* ]] && echo 1 || echo)}"
+: "${CFG_NVIM=$([[ -n $(command -v nvim) ]] && echo 1 || echo)}"
+: "${CFG_GNUPG_AGENT=}"
 
 while IFS= read -r var; do
     if [[ -n ${!var:-} ]]; then
