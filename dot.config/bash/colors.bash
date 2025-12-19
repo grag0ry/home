3ea853f7c191ca2a05a59604b818f7364fb9c643
Gregory Kudrenko <gragory.mail@gmail.com>
2025-12-19 10:40:52 +0100

    colors

---
+++ dot.config/bash/colors.bash
@@ -0,0 +1,55 @@
+export EXA_COLORS="\
+di=38;5;111:\
+fi=38;5;251:\
+ln=38;5;116:\
+ex=38;5;114:\
+da=38;5;240:\
+sn=38;5;180:sb=38;5;180:\
+ur=38;5;176:uw=38;5;176:ux=38;5;176:\
+gr=38;5;111:gw=38;5;111:gx=38;5;111:\
+tr=38;5;116:tw=38;5;116:tx=38;5;116:\
+uu=38;5;176:gu=38;5;111:\
+un=38;5;116:gn=38;5;116:\
+ga=38;5;114:\
+gm=38;5;180:\
+gd=38;5;176:\
+gv=38;5;116:\
+gt=38;5;111"
+
+export LS_COLORS="\
+di=38;5;111:\
+fi=38;5;251:\
+ln=38;5;116:\
+ex=38;5;114:\
+pi=38;5;176:\
+so=38;5;176:\
+bd=38;5;180;1:\
+cd=38;5;180;1:\
+or=38;5;176;1:\
+mi=38;5;176;1:\
+su=38;5;176;1:\
+sg=38;5;111;1:\
+tw=38;5;116;1:\
+ow=38;5;111;1:\
+st=38;5;116;1:\
+ca=38;5;176;1:\
+mh=38;5;180:\
+rs=0"
+
+# Upstream: github.com/bluz71/vim-moonfly-colors
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --color bg:#080808 \
+  --color bg+:#262626 \
+  --color border:#2e2e2e \
+  --color fg:#b2b2b2 \
+  --color fg+:#e4e4e4 \
+  --color gutter:#262626 \
+  --color header:#80a0ff \
+  --color hl+:#f09479 \
+  --color hl:#f09479 \
+  --color info:#cfcfb0 \
+  --color marker:#f09479 \
+  --color pointer:#ff5189 \
+  --color prompt:#80a0ff \
+  --color spinner:#36c692
+"
